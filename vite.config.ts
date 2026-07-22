0a26020079a5ce2767f6f810a044cf1a1c984a1f
caiye <caiye2531889700@gmail.com>
2026-07-22 09:21:03 +0800

    🐎 ci(打包): 打包分割修改

--- vite.config.ts
+++ vite.config.ts
@@ -7,30 +7,30 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
 	const env = loadEnv(mode, process.cwd(), 'VITE_');
 
-	const manualChunks = (id: string) => {
-		if (!id.includes('node_modules')) return;
+	// const manualChunks = (id: string) => {
+	// 	if (!id.includes('node_modules')) return;
 
-		if (
-			id.includes('@open-file-viewer') ||
-			id.includes('ag-psd') ||
-			id.includes('pdfjs-dist') ||
-			id.includes('mammoth') ||
-			id.includes('jszip') ||
-			id.includes('utif') ||
-			id.includes('tiff')
-		) {
-			return 'vendor-open-file-viewer';
-		}
-		if (id.includes('echarts') || id.includes('zrender')) return 'vendor-echarts';
-		if (id.includes('xlsx')) return 'vendor-xlsx';
-		if (id.includes('element-plus') || id.includes('@element-plus')) return 'vendor-element-plus';
-		if (id.includes('vue-router')) return 'vendor-vue-router';
-		if (id.includes('pinia')) return 'vendor-pinia';
-		if (id.includes('axios')) return 'vendor-axios';
-		if (id.includes('/vue/') || id.includes('\\vue\\')) return 'vendor-vue';
+	// 	if (
+	// 		id.includes('@open-file-viewer') ||
+	// 		id.includes('ag-psd') ||
+	// 		id.includes('pdfjs-dist') ||
+	// 		id.includes('mammoth') ||
+	// 		id.includes('jszip') ||
+	// 		id.includes('utif') ||
+	// 		id.includes('tiff')
+	// 	) {
+	// 		return 'vendor-open-file-viewer';
+	// 	}
+	// 	if (id.includes('echarts') || id.includes('zrender')) return 'vendor-echarts';
+	// 	if (id.includes('xlsx')) return 'vendor-xlsx';
+	// 	if (id.includes('element-plus') || id.includes('@element-plus')) return 'vendor-element-plus';
+	// 	if (id.includes('vue-router')) return 'vendor-vue-router';
+	// 	if (id.includes('pinia')) return 'vendor-pinia';
+	// 	if (id.includes('axios')) return 'vendor-axios';
+	// 	if (id.includes('/vue/') || id.includes('\\vue\\')) return 'vendor-vue';
 
-		return 'vendor';
-	};
+	// 	return 'vendor';
+	// };
 
 	return {
 		// base: '/adminManage/', // o2s.fun 网页用
@@ -58,65 +58,43 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
 						minSize: 20000,
 						groups: [
 							{
-								name: 'vendor-echarts',
-								test: /echarts/,
+								name: 'vendor-file',
+								test: /(@open-file-viewer|pdfjs-dist|mammoth|jszip|ag-psd|utif|tiff)/,
 								priority: 30
 							},
+
 							{
-								name: 'vendor-zrender',
-								test: /zrender/,
+								name: 'vendor-chart',
+								test: /(echarts|zrender)/,
 								priority: 30
 							},
-							{
-								name: 'vendor-open-file-viewer',
-								test: /open-file-viewer/,
-								priority: 20
-							},
+
 							{
 								name: 'vendor-xlsx',
 								test: /xlsx/,
 								priority: 20
 							},
+
 							{
-								name: 'vendor-element-plus',
+								name: 'vendor-ui',
 								test: /element-plus/,
 								priority: 20
 							},
+
 							{
-								name: 'vendor-vue-router',
-								test: /vue-router/,
+								name: 'vendor-framework',
+								test: /(vue|vue-router|pinia)/,
 								priority: 20
-							},
-							{
-								name: 'vendor-pinia',
-								test: /pinia/,
-								priority: 20
-							},
-							{
-								name: 'vendor-axios',
-								test: /axios/,
-								priority: 20
-							},
-							{
-								name: 'vendor-vue',
-								test: /vue/,
-								priority: 20
-							},
-							{
-								name: 'vendor',
-								test: /[\\/]node_modules[\\/]/,
-								priority: 10,
-								minSize: 30000,
 							},
 
 							{
-								name: 'common',
-								minShareCount: 2,
-								minSize: 10000,
-								priority: 5
+								name: 'vendor-common',
+								test: /[\\/]node_modules[\\/]/,
+								priority: 1,
+								minSize: 30000
 							}
 						]
-					},
+					}
 				}
 			}
 		},
