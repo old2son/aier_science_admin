4158628009ef74cfc6b3464d5782e01273143e4e
caiye <caiye2531889700@gmail.com>
2026-07-28 11:15:34 +0800

    🐞 fix(时间段修改): 时间段修改匹配上小程序

--- src/constants/timeSlots.ts
+++ src/constants/timeSlots.ts
@@ -5,7 +5,7 @@ export interface TimeSlotOption {
 
 export const TIME_SLOT_OPTIONS: TimeSlotOption[] = [
 	{ label: '09:00 - 10:30', value: '09:00-10:30' },
-	{ label: '10:30 - 11:30', value: '10:30-11:30' },
-	{ label: '14:30 - 15:30', value: '14:30-15:30' },
+	{ label: '10:30 - 12:00', value: '10:30-12:00' },
+	{ label: '14:30 - 16:00', value: '14:30-16:00' },
 	{ label: '16:00 - 17:00', value: '16:00-17:00' },
 ];
